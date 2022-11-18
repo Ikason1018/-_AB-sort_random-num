de9d76223b89da1cd0c9d62f1abe4dbaf9ee3ee4
Ikason1018 <76238601+Ikason1018@users.noreply.github.com>
2022-11-19 04:20:20 +0900

    不具合解消

--- main.c
+++ main.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
-int Elemcase = 3; // 要素数の種類
-int pattern = 10; // 要素数ごとの種類
+//パラメータ
+const int Elemcase = 3; // 要素数の種類
+const int pattern = 10; // 要素数ごとの種類
+const char* fname_pass = "randnum/"; //fnameのpass
+const int fname_max = 50; //fnameの文字数
 
 //乱数計算関数
-int rand_create(FILE *fp, int n, int init) {
+void rand_create(FILE *fp, int n, int init) {
   srand((unsigned int)time(NULL) + init);
   for(int i = 0;i < n;i++){
     fprintf(fp,"%d\n",rand());
   }
-  return 0;
 }
 
 int main(void) {
   //変数宣言
-  int n[Elemcase]; //要素数を格納
-  char fname[Elemcase * pattern][20];
   FILE *fp[Elemcase * pattern];
+  int n[Elemcase]; //要素数を格納
+  char fname[Elemcase * pattern][fname_max];
 
   //初期設定
   printf("Please enter the number of the %dth types of elements in order.\n", Elemcase);
@@ -27,7 +29,8 @@ int main(void) {
     scanf("%d[^\n]%*c", &n[i]);
     //出力ファイル名の指定
     for (int j = 0; j < pattern; j++) {
-      sprintf(fname[i * pattern + j], "%s%d%s%d%s", "randnum-", i, "-", j,".txt");
+      sprintf(fname[i * pattern + j], "%s%s%d%s%d%s", fname_pass, "randnum-", i, "-", j,".txt");
+      printf("fname[%d]:%s%s%d%s%d%s", i * pattern + j, fname_pass, "randnum-", i, "-", j,".txt\n");
     }
   }
 
